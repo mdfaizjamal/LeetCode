056c53647847fa8009db1cddb470af02fca78338
MD Faiz Jamal <126933736+mdfaizjamal@users.noreply.github.com>
2025-08-20 11:25:42 +0530

    Time: 0 ms (100%), Space: 10.5 MB (64.55%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(vector<int>& nums, int i)
+    // int dp[101];
+    /*int solve(vector<int>& nums, int i)
     {
         if(i>=nums.size())
         return 0;
@@ -13,9 +13,17 @@ public:
         int skip =  solve(nums, i+1);
 
         return dp[i] = max(steal, skip);
-    }
+    }*/
     int rob(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return solve(nums, 0);
+        // memset(dp, -1, sizeof(dp));
+       // return solve(nums, 0);
+       int n = nums.size();
+        vector<int> dp (n+1 , 0);
+        dp[0] = 0;
+        dp[1] = nums[0];
+       for(int i=2; i<=n ; i++){
+        dp[i] = max(dp[i-1], dp[i-2]+nums[i-1]);
+       }
+       return dp[n];
     }
 };
