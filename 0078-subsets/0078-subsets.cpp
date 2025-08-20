5734e78f9ed13c215e758076c512bb606d714c33
MD Faiz Jamal <126933736+mdfaizjamal@users.noreply.github.com>
2025-08-20 08:55:10 +0530

    Time: 0 ms (100%), Space: 8.3 MB (100%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void help(int i, vector<int>& nums, vector<int>&temp,vector<vector<int>>& ans)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(temp);
+        }
+        else
+        {
+            // include ith element
+            temp.push_back(nums[i]);
+            help(i+1,nums,temp,ans);
+
+            temp.pop_back();
+            //without taking ith element
+            help(i+1,nums,temp,ans);
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int>temp;
+        help(0,nums,temp,ans);
+        return ans;
+    }
+};
