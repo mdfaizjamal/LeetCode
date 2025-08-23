368a9bb343a2c2b394ca150af0b787cb01834721
MD Faiz Jamal <126933736+mdfaizjamal@users.noreply.github.com>
2025-08-23 13:05:14 +0530

    Time: 11 ms (90.52%), Space: 45.8 MB (69.8%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin() , people.end());
+        int n = people.size();
+        int i =0 , j =n-1 , result =0;
+
+        while(i <= j){
+            if(i != j && people[i] + people[j] <= limit){
+                result++;
+                i++;
+                j--;
+            }
+            else
+            {
+                result++;
+                j--;
+            }
+        }
+        return result;
+    }
+};
